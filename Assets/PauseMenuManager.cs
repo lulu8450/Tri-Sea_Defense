732e5716130303b143b6ce55c7b9560914caf786
L.Boyer <boyerlucas29@gmail.com>
2025-09-12 14:30:14 +0400

    Reapply "Merge branch 'Daniel' into ALPHA"
    This reverts commit 40bf380764f3f96a86c0e9bcfdb73fe0c9887838.

---
+++ Assets/PauseMenuManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    private GameObject pauseMenuUI;
+    private Button homeButton;
+    private Button resumeButton;
+    private Button quitButton;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+
+        // Récupère le parent "Pause"
+        pauseMenuUI = GameObject.Find("Pause");
+        if (pauseMenuUI == null)
+        {
+            return;
+        }
+
+        pauseMenuUI.SetActive(false);
+
+        // Récupère les enfants (Home, Resume, Quit)
+        homeButton   = pauseMenuUI.transform.Find("Home").GetComponent<Button>();
+        resumeButton = pauseMenuUI.transform.Find("Resume").GetComponent<Button>();
+        quitButton   = pauseMenuUI.transform.Find("Quit").GetComponent<Button>();
+
+        // Ajoute les events
+        homeButton.onClick.AddListener(GoHome);
+        resumeButton.onClick.AddListener(Resume);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Échap pressé ⌨️");
+
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    void GoHome()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu"); 
+    }
+
+    void QuitGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;    }
+}
